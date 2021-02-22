578ec6f35ad62e236934a71eb6b6dd24abf1cedd
Mufafafa <79324773+Mufafafa@users.noreply.github.com>
2021-02-22 09:46:46 -0600

    Add files via upload

---
+++ 2.22.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a, b, c, d;
+	printf("%d%d%d", a, b, c);
+
+
+	//int a;
+	//scanf("%d", &a);
+	//switch (a)
+	//{
+	//case 1://这是冒号！！！
+	//case 2://情况相同可省略
+	//case 3:
+	//case 4:
+	//case 5:
+	//	printf("工作日");
+	//	break;
+	//case 6:
+	//case 7:
+	//	printf("休息日");
+	//	break;//好习惯，最后也写个break
+	//default://default可有可无，根据需要
+	//	printf("输入错误");
+	//	break;
+	//}
+	
+
+	/*int a;
+	int i=1;
+	scanf("%d", &a);
+	if (a % 2 == 0)
+		printf("偶数\n");
+	else
+		printf("奇数\n");
+	while(i <= 100)
+	{
+		if(i%2==1)
+			printf("%d  ", i);
+		i = i++;
+	}*/
+
+
+	//int a = 20;
+	//if(a < 18)
+	//{
+	//	printf("未成年");
+	//	printf("不能谈恋爱");
+	//}
+	//else if(a >= 18 && a < 28)
+	//		printf("青年");
+	//else if(a >= 28 && a < 50)
+	//		printf("壮年");
+	//else
+	//		printf("老年");
+	return 0;
+}
