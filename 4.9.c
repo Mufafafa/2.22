deafed5b9f8fc09414d3c8ee745a44bf88c17ab8
Mufafafa <79324773+Mufafafa@users.noreply.github.com>
2021-04-13 12:40:12 +0800

    Add files via upload

---
+++ 4.9.c
@@ -0,0 +1,222 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//法一
+//int count_bit_one(unsigned int n)//注意unsigned,否则n若是负数则结果不对
+//{
+//	int count = 0;
+//	while (n)
+//	{
+//		if (n % 2 == 1)
+//		{
+//			count++;
+//		}
+//		n = n / 2;
+//	}
+//	return count;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	//写一个函数求a的二进制（补码）表示中有几个1
+//	int count = count_bit_one(a);
+//	printf("%d", count);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//法二
+//int count_bit_one(unsigned int n)
+//{
+//	int count = 0;
+//	int i = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if (((n >> 1) & 1) == 1)
+//		{
+//			count++;
+//		}
+//	}
+//	return count;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	//写一个函数求a的二进制（补码）表示中有几个1
+//	int count = count_bit_one(a);
+//	//10000000000000000000000000000001   -1的原码
+//	//11111111111111111111111111111110   反码
+//	//11111111111111111111111111111111   补码
+//	//00000000000000000000000000000001
+//	printf("%d", count);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int count_bit_one(unsigned int n)
+//{
+//	int count = 0;
+//	int i = 0;
+//	while (n)
+//	{
+//		n = n & (n - 1);
+//		count++;
+//	}
+//	return count;
+//}
+////n = n & (n - 1)
+////n=13
+////1101 n
+////1100 n-1
+////1100 n
+////1011 n-1
+////1000 n
+////0111 n-1
+////0000 n
+//
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	//写一个函数求a的二进制（补码）表示中有几个1
+//	int count = count_bit_one(a);
+//	printf("%d", count);
+//	return 0;
+//}
+
+
+
+//获取一个整数二进制序列中所有的偶数位和奇数位
+//#include<stdio.h>
+//void print(int m)
+//{
+//	int i = 0;
+//	printf("奇数位：\n");
+//	for (i = 30; i >= 0; i -= 2)
+//	{
+//		printf("%d", (m >> i) & 1);
+//	}
+//	printf("\n");
+//	printf("偶数位：\n");
+//	for (i = 31; i >= 1; i -= 2)
+//	{
+//		printf("%d", (m >> i) & 1);
+//	}
+//	printf("\n");
+//}
+//
+//int main()
+//{
+//	int m = 0;
+//	scanf("%d", &m);
+//	print(m);
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+//
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//
+//void reverse_string(char arr[])
+//{
+//	int left = 0;
+//	int right = my_strlen(arr) - 1;
+//	while (left < right)
+//	{
+//		int tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	reverse_string(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//
+//void reverse_string(char arr[])
+//{
+//	char tmp = arr[0];
+//	int len = my_strlen(arr);
+//	arr[0] = arr[len - 1];
+//	arr[len - 1] = '\0';
+//	if (my_strlen(arr + 1) >= 2)
+//	{
+//		reverse_string(arr + 1);
+//	}
+//	arr[len - 1] = tmp;
+//}
+//
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	reverse_string(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+////DigitSum(1729)
+////DigitSum(172)+1729%10
+////DigitSum(17)+172%10+1729%10
+////DigitSum(1)+17%10+172%10+1729%10
+////1+7+2+9
+//int DigitSum(unsigned int num)
+//{
+//	if (num > 9)
+//	{
+//		return DigitSum(num / 10) + num % 10;
+//	}
+//	else
+//	{
+//		return num;
+//	}
+//}
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%d", &num);
+//	int ret = DigitSum(num);
+//	printf("ret=%d\n", ret);
+//	return 0;
+//}
+
+
+
+
+
